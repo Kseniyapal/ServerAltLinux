10ac1370b496e7dfcee51cc59ae1bfff8df1fa57
Kseniyapal <100351525+Kseniyapal@users.noreply.github.com>
2024-05-17 21:45:43 +0300

    Create recovery_invoiceninja_data_script.sh

---
+++ etherpad/recovery_invoiceninja_data_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 db_psswd file"
+  exit 1
+fi
+
+# Получаем директорию текущего скрипта
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Проверка существования пользователя базы данных
+USER_EXISTS=$(mysql -u root -p"$1" -e "SELECT User FROM mysql.user WHERE User='invoiceninjauser';" | grep invoiceninjauser)
+
+# Проверка существования базы данных
+DB_EXISTS=$(mysql -u root -p"$1" -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='invoiceninja';" | grep invoiceninja)
+
+# Восстановление утерянных фрагметов
+if [ -z "$DB_EXISTS" ] || [ -z "$USER_EXISTS" ]; then
+  mysql -u root -p"$1" --force < "$SCRIPT_DIR/db_setup_invoiceninja_with_arg.sql"
+fi
+
+# Восстановление базы данных из копии
+mysql -u root -p"$1" invoiceninja < "$2"
+
+systemctl restart httpd2.service
+
+# Сообщение об успешном восстановлении базы данных
+echo "База данных успешно восстановлена из файла $2"
