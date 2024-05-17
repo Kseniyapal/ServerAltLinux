1aa6d21a74ecd296e913013dafd9e607b11cc658
Kseniyapal <100351525+Kseniyapal@users.noreply.github.com>
2024-05-17 21:43:45 +0300

    Create dump_invoiceninja_data_script.sh

---
+++ etherpad/dump_invoiceninja_data_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 db_psswd directory"
+  exit 1
+fi
+
+# Создание каталога для сохранения копии, если он не существует
+mkdir -p "$2"
+
+# Получение текущей даты в формате ГГГГ-ММ-ДД
+date=$(date +"%Y-%m-%d")
+
+# Выполнение mysqldump и сохранение копии в файл
+mysqldump -u root -p"$1" invoiceninja > "$2/dump_invoiceninja_data_$date.sql"
+
+# Сообщение об успешном создании копии
+echo "Копия базы данных успешно создана в файле " "$2/dump_invoiceninja_data_$date.sql"
